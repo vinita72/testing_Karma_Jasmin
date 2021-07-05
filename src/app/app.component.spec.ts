3c18147a4e1799ce16f417a0146449d3eba3beb3
vinita72 <imvinitarajoria@gmail.com>
2021-07-05 12:43:59 +0530

    commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,73 @@
-import { TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { TestBed, ComponentFixture, fakeAsync, waitForAsync, inject } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule, By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  let comp:AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
+
+  beforeEach(waitForAsync (() => {
+     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule
       ],
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
-  });
+    }).compileComponents().then(()=>{
+      fixture = TestBed.createComponent(AppComponent);
+    comp = fixture.componentInstance;
+    de=fixture.debugElement.query(By.css('form'));
+    el=de.nativeElement;
+    });
+  }));
 
-  it('should create the app', () => {
+  it('should create the app', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
+  }));
 
-  it(`should have as title 'my-app'`, () => {
+  it(`should have as title 'my-app'`, waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('my-app');
+  }));
+
+
+
+
+  it('should set submitted to true', waitForAsync(() => {
+    comp.onSubmit();
+    expect(comp.submitted).toBeTruthy();
+  }));
+
+  it(`should call the onSubmit method`, () => {
+     fixture.detectChanges();
+     spyOn(comp, 'onSubmit');
+     el=fixture.debugElement.query(By.css('button')).nativeElement;
+     el.click();
+    expect(comp.onSubmit).toHaveBeenCalledTimes(0);
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-app app is running!');
-  });
+
+
+  it('form should be invalid', waitForAsync(() => {
+    comp.contactForm.controls['username'].setValue('');
+    comp.contactForm.controls['password'].setValue('');
+    expect(comp.contactForm.valid).toBeFalsy();
+  }));
+  it('form should be valid', waitForAsync(() => {
+    comp.contactForm.controls['username'].setValue('vachi');
+    comp.contactForm.controls['password'].setValue('vachi12');
+    expect(comp.contactForm.valid).toBeTruthy();
+  }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  contactForm!:FormGroup;
+  contact={
+    username:'',
+    password:''
+  };
+  submitted=false;
+  constructor() { 
+    this.creatForm();
+  }
+  creatForm():void {
+
+this.contactForm = new FormGroup({
+'username': new FormControl(this.contact.username,[
+  Validators.required,
+  Validators.minLength(4)
+]),
+'password': new FormControl(this.contact.password,[
+  Validators.required,
+  Validators.minLength(4)
+]),
+
+});    
+  }
+
+  onSubmit():void{
+  this.submitted = true;
+}
+
+  
 }
